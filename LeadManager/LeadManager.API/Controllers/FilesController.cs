be518d3a28729ca1224e67c0131f18df37e29fd9
Malith Jayawardana <malithj@gmail.com>
2022-11-06 19:40:38 +0000

    WIP: Added support to download files
    Renamed SourceController to SourcesController
    Added support for responses in XML format
    Handled requests for unsupported response formats

---
+++ LeadManager/LeadManager.API/Controllers/FilesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace LeadManager.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        //public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        //{
+        //    _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+        //        ?? throw new System.ArgumentNullException(
+        //            nameof(fileExtensionContentTypeProvider));
+        //}
+
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            //The logic to lookup the file path needs to be written
+            //This is just for demo porposes
+            string pathToFile = "Files/sns-menukort-2022-08-uk-150x380-low.pdf";
+
+            //First check if the file exists, if not return a not found response
+            if (!System.IO.File.Exists(pathToFile))
+                return NotFound();
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+            //if (!_fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+            //    contentType = "application/octet-stream";
+
+            return File(bytes, "txt/plain", Path.GetFileName(pathToFile));
+            
+        }
+
+    }
+}
--- LeadManager/LeadManager.API/Program.cs
+++ LeadManager/LeadManager.API/Program.cs
@@ -3,7 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 //You could also do this using Services.AddMvc/Services.AddMvcCore/Services.AddControllersWithViews,
 //However these will add additional services that are not required for an API such as Razor view support 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    //If Accept header value in the request is not supported, give back a 406 response saying it is not supported
+    options.ReturnHttpNotAcceptable = true; 
+} 
+).AddXmlDataContractSerializerFormatters(); //Add support for responses in XML format 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
