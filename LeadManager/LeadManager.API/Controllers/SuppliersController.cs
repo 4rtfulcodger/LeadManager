77e545ed81662c68aad5baefe392154113d377ae
Malith Jayawardana <malithj@gmail.com>
2023-07-15 13:42:55 +0100

    Started using the API endpoint handler in SuppliersController.

--- LeadManager/LeadManager.API/Controllers/SuppliersController.cs
+++ LeadManager/LeadManager.API/Controllers/SuppliersController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using LeadManager.Core.Entities;
+using LeadManager.Core.Entities.Source;
 using LeadManager.Core.Entities.Supplier;
 using LeadManager.Core.Interfaces;
 using LeadManager.Core.Interfaces.Supplier;
@@ -17,93 +18,74 @@ namespace LeadManager.API.Controllers
         private readonly IEmailService _emailService;
         private readonly IMapper _mapper;
         public ISupplierService _supplierService;
+        private readonly IApiEndpointHandler _apiEndpointHandler;
 
         public SuppliersController(ISupplierService supplierService,
             ILogger<FilesController> logger,
             IEmailService emailService,
-            IMapper mapper)
+            IMapper mapper,
+            IApiEndpointHandler apiEndpointHandler)
         {
             _supplierService = supplierService ?? throw new ArgumentException(nameof(supplierService)); ;
             _logger = logger ?? throw new ArgumentException(nameof(logger));
             _emailService = emailService ?? throw new ArgumentException(nameof(emailService));
             _mapper = mapper;
+            _apiEndpointHandler = apiEndpointHandler;
         }
 
 
         [HttpGet()]
         public async Task<IActionResult> GetSuppliers()
         {
-            return Ok(_mapper.Map<SupplierDto[]>(await _supplierService.GetSuppliersAsync()));
+            var getResult = await _supplierService.GetSuppliersAsync();
+            return _apiEndpointHandler.ReturnSearchResult<IEnumerable<Supplier>, SupplierDto[]>(getResult);            
         }
 
         [HttpGet("{id}", Name = "GetSupplier")]
         public async Task<IActionResult> GetSupplier(int id)
         {
             var supplier = await _supplierService.GetSupplierWithIdAsync(id);
-
-            if (supplier == null)
-                return NotFound();
-
-            return Ok(supplier);
+            return _apiEndpointHandler.ReturnSearchResult<Supplier, SupplierDto>(supplier);
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateSupplier(SupplierForCreateDto supplierDto)
         {
             var newSupplier = _mapper.Map<Supplier>(supplierDto);
-            bool addLeadSuccess = await _supplierService.AddSupplierAsync(newSupplier);
 
-            return CreatedAtRoute("GetSource", new { id = newSupplier.SupplierId }, _mapper.Map<SupplierDto>(newSupplier));
-
+            return _apiEndpointHandler.ReturnCreateResult<SupplierDto>(await _supplierService.AddSupplierAsync(newSupplier),
+                "GetSupplier",
+                newSupplier.SupplierId.ToString(),
+                newSupplier);
         }
 
         [HttpPatch("{supplierId}")]
         public async Task<IActionResult> UpdateSupplier(JsonPatchDocument<SupplierForUpdateDto> patchDocument, int supplierId)
         {
             var supplierEntity = await _supplierService.GetSupplierWithIdAsync(supplierId);
-            if (supplierEntity == null)
-                return NotFound();
+
+            if (!_apiEndpointHandler.IsValidEntitySearchResult<Supplier>(supplierEntity))
+                return BadRequest();            
 
             var supplierDto = _mapper.Map<SupplierForUpdateDto>(supplierEntity);
             patchDocument.ApplyTo(supplierDto);
-
             _mapper.Map(supplierDto, supplierEntity);
-            bool updateresult = await _supplierService.UpdateSupplierAsync(supplierId);
 
-            if (updateresult)
-            {
-                return NoContent();
-            }
-            else
-            {
-                return Problem();
-            }
+            return _apiEndpointHandler.ReturndUpdateResult(await _supplierService.UpdateSupplierAsync(supplierId));           
 
         }
 
         [HttpDelete("{id}", Name = "DeleteSupplier")]
         public async Task<IActionResult> DeleteSupplier(int id)
         {
-            //Validation and filter logic should be removed from controllers
-            //Temporarily adding these for testing
-
             _logger.Log(LogLevel.Debug, "Request to SuppliersController, DeleteSupplier action");
 
             var supplierToDelete = await _supplierService.GetSupplierWithIdAsync(id);
 
-            if (supplierToDelete == null)
-                return NotFound();
+            if (!_apiEndpointHandler.IsValidEntitySearchResult<Supplier>(supplierToDelete))
+                return BadRequest();
 
-            var deleteResult = await _supplierService.DeleteSupplier(supplierToDelete);
-
-            if (deleteResult)
-            {
-                return NoContent();
-            }
-            else
-            {
-                return Problem();
-            }
+            return _apiEndpointHandler.ReturnDeleteResult(await _supplierService.DeleteSupplier(supplierToDelete));
         }
     }
 }
