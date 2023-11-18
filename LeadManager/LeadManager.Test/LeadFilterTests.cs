924b9ebb91b7a0dfba25ee2548409a3441f59d4f
Malith Jayawardana <malithj@gmail.com>
2023-11-18 14:40:57 +0000

    Added Unit test project to solution

---
+++ LeadManager/LeadManager.Test/LeadFilterTests.cs
@@ -0,0 +1,29 @@
+﻿using LeadManager.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadManager.Test
+{
+    public class LeadFilterTests
+    {
+        [Fact]
+        public void LeadHelper_LeadFilter_HasDefaultValuesSetForPageNumberAndPageSize() {
+
+            LeadFilter leadFilter = new LeadFilter();
+
+            Assert.Equal(1, leadFilter.PageNumber);
+            Assert.Equal(5, leadFilter.PageSize);
+        }
+
+        [Fact]
+        public void LeadHelper_LeadFilter_WillResetPageSizeTo20WhenAttemptingToSetAValueHigherThan20()
+        {
+            LeadFilter leadFilter = new LeadFilter();
+            leadFilter.PageSize = 21;
+            Assert.Equal(20, leadFilter.PageSize);
+        }
+    }
+}
